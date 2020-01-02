18212f5e528f344e59975a84ddf0ae455c8857ab
kirill <kirya.smart.03@gmail.com>
2020-01-02 13:36:26 +0700

    added class Player without intersections check

--- game/main.py
+++ game/main.py
@@ -1,4 +1,6 @@
+'''Hey, this is Catacombs: the game *_* '''
 import pygame
+import math
 
 
 class Creature():
@@ -20,7 +22,29 @@ class Creature():
 
 class Player(Creature):
     def __init__(self, x, y, pov):
-        super().__init__(x,y,pov)
+        super().__init__(x, y, pov)
+        self.walkSpeed = 32 / 60
+        self.rotateSpeed = 0.5
+
+    def rotate_left(self):
+        if self.pov + self.rotateSpeed == 360:
+            self.pov = 0
+        else:
+            self.pov += self.rotateSpeed
+
+    def rotate_right(self):
+        if self.pov - self.rotateSpeed < 0:
+            self.pov = 359.5
+        else:
+            self.pov -= self.rotateSpeed
+
+    def walk_forward(self):
+        self.x += math.cos(math.radians(self.pov)) * self.walkSpeed
+        self.y += math.sin(math.radians(self.pov)) * self.walkSpeed
+
+    def walk_back(self):
+        self.x -= math.cos(math.radians(self.pov)) * self.walkSpeed
+        self.y -= math.sin(math.radians(self.pov)) * self.walkSpeed
 
 
 class Area():
