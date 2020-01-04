3f4659f4248308f44265122829babb5cece70138
kirill <kirya.smart.03@gmail.com>
2020-01-04 18:49:33 +0700

    chertovshina

--- game/main.py
+++ game/main.py
@@ -86,32 +86,42 @@ class Area():
         CAM_TO_PP = PP[0] / 2 / tan(radians(self.player.fov / 2))
         ANGLE_BETWEEN_RAYS = self.player.fov / PP[0]
         angle_bet = -30
-        angle = self.player.pov - self.player.fov / 2
+        angle = self.player.pov + self.player.fov / 2
         for i in range(PP[0]):
             if self.player.view_up:
-                intesection_yy = floor(self.player.y / 64) * 64 - 1
+                intesection_yy = self.player.y // 64 * 64 - 1
                 yy_next = -64
             else:
-                intesection_yy = floor(self.player.y / 64) * 64 + 64
+                intesection_yy = self.player.y // 64 * 64 + 64
                 yy_next = 64
             if self.player.view_right:
-                intesection_xx = floor(self.player.x / 64) * 64 + 64
+                intesection_xx = self.player.x // 64 * 64 + 64
                 xx_next = 64
             else:
-                intesection_xx = floor(self.player.x / 64) * 64 - 1
+                intesection_xx = self.player.x // 64 * 64 - 1
                 xx_next = -64
             cosinus, tangens = cos(radians(angle)), tan(radians(angle))
             intesection_yx = self.player.x + (self.player.y - intesection_yy) / tangens
-            yx_next = 64 / tangens
+            if self.player.view_right and self.player.view_up:
+                yx_next = 64 / tangens
+                xy_next = 0 - 64 * tangens
+            elif self.player.view_right and not self.player.view_up:
+                yx_next = 0 - 64 / tangens
+                xy_next = 0 - 64 * tangens
+            elif not self.player.view_right and self.player.view_up:
+                yx_next = 64 / tangens
+                xy_next = 64 * tangens
+            elif not self.player.view_up and not self.player.view_right:
+                yx_next = 0 - 64 / tangens
+                xy_next = 64 * tangens
             intesection_xy = self.player.y + (self.player.x - intesection_xx) * tangens
-            xy_next = 64 * tangens
             intersected = False
             distance = []
             while not intersected:
                 try:
                     x = int(intesection_yx / 64)
                     y = int(intesection_yy / 64)
-                    #print(x, y)
+                    # print(x, y)
                     if self.map[y][x] == 1:
                         distance.append(
                             sqrt((self.player.x - intesection_yx) ** 2 + (self.player.y - intesection_yy) ** 2))
@@ -138,9 +148,13 @@ class Area():
                     break
             distance = min(distance) * cos(radians(angle_bet))
             high = int(64 / distance * CAM_TO_PP)
-            pygame.draw.line(self.screen, (0, 0, 0), (PP[0] - i, int(PP[1] / 2 - high / 2)), (PP[0]-i,  int(PP[1] / 2 - high / 2)+high))
-
-            angle += ANGLE_BETWEEN_RAYS
+            pygame.draw.line(self.screen, (0, 0, 0), (i, int(PP[1] / 2 - high / 2)),
+                             (i, int(PP[1] / 2 - high / 2) + high))
+            if distance < 1 and angle == self.player.pov:
+                self.player.walk_speed = 0
+            else:
+                self.player.walk_speed = 10
+            angle -= ANGLE_BETWEEN_RAYS
             angle_bet += ANGLE_BETWEEN_RAYS
 
 
@@ -151,7 +165,7 @@ wall_surface.fill((255, 255, 255))
 running = True
 FPS_CONTROL = 30
 pygame.time.set_timer(FPS_CONTROL, 50)
-player = Player(96, 416, 46)
+player = Player(464.42276512364344, 374.01080205950484, 1.5655579999999532)
 print(player.view_right, player.view_up)
 world = Area(player, wall_surface)
 world.show()
@@ -177,7 +191,7 @@ while running:
                 left = False
             elif event.key == pygame.K_RIGHT:
                 right = False
-            print(player.pov, player.x, player.y)
+            print(player.x, player.y, player.pov)
         if event.type == pygame.QUIT:
             running = False
         if event.type == FPS_CONTROL:
